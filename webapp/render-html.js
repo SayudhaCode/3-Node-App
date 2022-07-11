512d424facbd2f2592883b63575adab2aa156123
sayudhalw <mazaya.dev@gmail.com>
2022-07-11 13:49:58 +0700

    + Memanggil CSS dari NodeJS

--- webapp/render-html.js
+++ webapp/render-html.js
@@ -1,6 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
+function renderCSS(req, res) {
+	if (req.url === '/style.css') {
+		res.writeHead(200, { 'Content-Type': 'text/css' });
+		var fileContents = fs.readFileSync('./style.css', { encoding: 'utf-8' });
+		res.write(fileContents);
+		res.end();
+	}
+}
 function renderHtml(path, res) {
 	fs.readFile(path, null, (err, data) => {
 		if (err) {
@@ -14,6 +22,7 @@ function renderHtml(path, res) {
 }
 
 var server = http.createServer((req, res) => {
+	renderCSS(req, res);
 	res.writeHead(200, { 'Content-Type': 'text/html' });
 	renderHtml('./index.html', res);
 });
